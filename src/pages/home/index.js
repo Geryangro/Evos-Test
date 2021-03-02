9c3deaf4f816c932c736b1d376b8531d5d38b601
Gery <geryangro@gmail.com>
2021-03-02 18:00:42 +0700

    search query

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,7 @@ import { Waypoint } from 'react-waypoint';
 import { get } from '@service';
 import { queryStringify, removeEmptyAttributes, findIdPage } from "@helpers";
 import Card from "@component/card"
+import magnifier from '../../magnifier.svg';
 
 function Home() {
     const [data, setData] = useState([])
@@ -10,22 +11,22 @@ function Home() {
     const [currentPage, setCurrentPage] = useState(1)
     const [totalPage, setTotalPage] = useState(0)
     const [totalData, setTotalData] = useState(0)
+    const [query, setQuery] = useState('')
 
     const fetchData = async () => {
         setLoading(true)
         try {
             const res = await get(`/people/${queryStringify(removeEmptyAttributes({
                 page: totalPage,
-                // search: query
+                search: query
             }))}`)
             setData(res.data.results)
             const nextPage = res.data.next
-            // const amountPage = nextPage.replace(/\D/g, '');
             setTotalData(res.data.results.length)
             setTotalPage(parseInt(findIdPage(nextPage)))
-            console.log(totalPage, 'total page')
-        } catch {
+        } catch(error) {
             setLoading(false)
+            console.log(error)
         }
         setLoading(false)
     }
@@ -35,13 +36,26 @@ function Home() {
         try {
             const resLoadMore = await get(`/people/${queryStringify(removeEmptyAttributes({
                 page: totalPage,
-                // search: query
+                search: query
             }))}`)
             const nextPage = resLoadMore.data.next
-            // const amountPageLoadMore = nextPage.replace(/\D/g, '');
             setData([...data, ...resLoadMore.data.results])
             setTotalData(totalData + resLoadMore.data.results.length)
             setTotalPage(parseInt(findIdPage(nextPage)))
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    const searchPeople = async (query) => {
+        setQuery(query)
+        try {
+            const res = await get(`/people/${queryStringify(removeEmptyAttributes({
+                page: 0,
+                search: query
+            }))}`)
+            setData(res.data.results)
+            setTotalData(totalData + res.data.results.length)
         } catch(error) {
             console.log(error)
         }
@@ -55,6 +69,10 @@ function Home() {
         <div className="App">
             {!loading && (
                 <>
+                    <div className="flex justify-between w-full items-center relative mt-8 mb-12">
+                        <img src={magnifier} alt="icon-magnifier" className="icon" />
+                        <input type="text" className="w-full btn-search-cstm" onChange={e => searchPeople(e.target.value)} />
+                    </div>
                     <div className="grid grid-cols-2">
                         {data ? data.map((item, key) => {
                             return (
